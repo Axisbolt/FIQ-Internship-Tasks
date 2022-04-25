7eb1f846553a1dfa6155ae95e5ee79ff31a4314e
Kapil Chouhan <37921622+Axisbolt@users.noreply.github.com>
2022-04-25 22:24:57 +0530

    Commit

---
+++ ASP.NET/Task Week 1 Day 1/DataAccessLayer/IEmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IEmployeeRepository
+    {
+        public Task<List<Employee>> GetEmployees();
+
+        public Task<Employee> GetEmployee(int id);
+
+        public Task InsertEmployee(Employee employee);
+        public Task UpdateEmployee(Employee employee);
+
+        public Task DeleteEmployee(int id);
+
+
+
+
+    }
+}
---
+++ ASP.NET/Task Week 1 Day 1/Task1/BusinessLayer/IEmployeeBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface IEmployeeBusiness
+    {
+        public Task<List<Employee>> GetEmployees();
+        public Task<Employee> GetEmployee(int id);
+        public Task InsertEmployee(Employee employee);
+        public Task UpdateEmployee(Employee employee);
+        public Task DeleteEmployee(int id);
+    }
+}

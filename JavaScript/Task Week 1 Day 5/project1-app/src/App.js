86b05d9bb684646235bf3fcffe38cda58a26a407
Kapil Chouhan <37921622+Axisbolt@users.noreply.github.com>
2022-05-20 01:20:33 +0530

    Commit

---
+++ JavaScript/Task Week 1 Day 5/project1-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, My Name is Kapil Chouhan 
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/kapil-chouhan-4738b9199/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

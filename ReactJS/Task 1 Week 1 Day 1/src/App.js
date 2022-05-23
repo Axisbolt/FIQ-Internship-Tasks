ffc0fbc9f12cb3692bd7caf72003a9d7b3981eef
Kapil Chouhan <37921622+Axisbolt@users.noreply.github.com>
2022-05-23 22:18:36 +0530

    commit

---
+++ ReactJS/Task 1 Week 1 Day 1/src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+
+function App() {
+  const name = 'Kapil'
+  const x = true
+  return (
+    <div className="container">
+   <Header />
+    {/* <h1>Hello My Name is Kapil Chouhan</h1>  
+   <h2>Hello My Name is {name}</h2>
+   <h2>Hello {2+2}</h2>
+   <h2>Hello {x ? 'Yes' : 'No'}</h2> */}
+   
+    </div>
+  );
+}
+
+export default App;
